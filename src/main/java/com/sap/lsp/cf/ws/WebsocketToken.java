4ce4cb06c860cb9190d5f11d0059a054d7c4d82b
Amiram Wingarten <amiram.wingarten@sap.com>
2017-07-11 08:46:12 +0300

    fix token servlet (#26)

--- src/main/java/com/sap/lsp/cf/ws/WebsocketToken.java
+++ src/main/java/com/sap/lsp/cf/ws/WebsocketToken.java
@@ -1,15 +1,22 @@
 package com.sap.lsp.cf.ws;
 
-import java.util.Date;
-
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.ws.rs.POST;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Logger;
 
-@WebServlet(description = "Update token ", urlPatterns = { "/UpdateToken/*" })
-public class WebsocketToken {
+@WebServlet(description = "Update token ", urlPatterns = {"/UpdateToken/*"})
+public class WebsocketToken extends HttpServlet {
+    private static final Logger LOG = Logger.getLogger(WSSynchronization.class.getName());
 
-	@POST
-	public void update(String token, Date timestamp) {
-		WebsocketTokenValidator.getInstance().update(token, timestamp);
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Long timestamp = Long.parseLong(request.getParameter("expiration"));
+        String token = request.getParameter("token");
+        LOG.info("UpdateToken was called with token " + token + " expiration date " + timestamp);
+        WebsocketTokenValidator.getInstance().update(token, new Date(timestamp));
+    }
 }
